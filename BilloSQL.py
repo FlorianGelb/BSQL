9fec7e0d869e9307a501ddc8a42545ab3fc8bf0c
NoopDoge <irolori64@gmail.com>
2019-10-29 17:15:43 +0100

    Keys can now be integers and * is valid

--- BilloSQL.py
+++ BilloSQL.py
@@ -30,12 +30,14 @@ def tokenizer(file):
                 c[j] = c[j].upper()
             for key in nterminale:
                 b = c[j]
-                if b.split(";")[0] in nterminale.get(key):
+                if b in nterminale.get(key):
                     tokens.append(key)
             if list(c[j])[-1] == ";" and key != terminale[0] and not ("\'" in list(c[j]) or "\"" in list(c[j])):#(list(c[j])[0] == "\"" or list(c[j])[0] == "\'" or list(c[j])):
                 tokens.append(terminale[1])
             if list(c[j])[0] == "\"" and list(c[j])[-1] == "\"" or list(c[j])[0] == "\'"  and list(c[j])[-1] == "\'" \
                     and len(list(c[j])) > 2:
+                tokens.append(terminale[1])
+            if c[j] in [str(n) for n in range(10)]:
                 tokens.append(terminale[1])
 
         if(grammar_check()):
@@ -50,11 +52,11 @@ def grammar_check():
     buffer = None
     order = 0
     for i in range(len(tokens)):
-        if buffer == None and tokens[i] == terminale[0]:
+        if buffer is None and tokens[i] == terminale[0]:
             order = 1
-        elif buffer == None and tokens[i] != terminale[0]:
+        elif buffer is None and tokens[i] != terminale[0]:
             return 0
-        if buffer != None:
+        if buffer is not None:
             if tokens[i] == terminale[0]:
                 order += 1
             if tokens[i] == terminale[1]:
@@ -82,7 +84,7 @@ def parser():
             pass
 
     if content_buffer[0].upper() == "SELECT":
-        print(DB.get(content_buffer[1].replace("\'", "").replace("\"", ""), content_buffer[content_buffer.index("FROM") + 1].split(";")[0]))
+        print(DB.get(content_buffer[1], content_buffer[content_buffer.index("FROM") + 1].split(";")[0]))
         tokens = []
     else:
         print("Anweisung ist semantisch sinnlos")
